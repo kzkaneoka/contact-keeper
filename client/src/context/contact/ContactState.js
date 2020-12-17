8f0c668a0e19729345ab5adeb2db1bb79a59420a
kzkaneoka <kzkaneoka@gmail.com>
2020-12-17 19:10:13 +0900

    Added form

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,83 @@
+import React, { useReducer } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from '../types';
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Jill Johnson',
+        email: 'jill@gmail.com',
+        phone: '111-111-1111',
+        type: 'personal',
+      },
+      {
+        id: 2,
+        name: 'Sara Watson',
+        email: 'sara@gmail.com',
+        phone: '222-222-2222',
+        type: 'personal',
+      },
+      {
+        id: 3,
+        name: 'Harry White',
+        email: 'harry@gmail.com',
+        phone: '333-333-3333',
+        type: 'professional',
+      },
+    ],
+    current: null,
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  const addContact = (contact) => {
+    contact.id = uuidv4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
+
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
+
+  const setCurrent = (contact) => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
+
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
+
+  const updateCurrent = (contact) => {
+    dispatch({ type: UPDATE_CURRENT, payload: contact });
+  };
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+        current: state.current,
+        addContact,
+        updateCurrent,
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
