2f6300042ea2f244afeef66a65c82bc045d568c0
kzkaneoka <kzkaneoka@gmail.com>
2020-12-20 14:58:55 +0900

    Communicate client and server to update contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -48,6 +48,24 @@ const ContactState = (props) => {
     }
   };
 
+  const updateContact = async (contact) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
+  };
+
   const deleteContact = async (id) => {
     try {
       const res = await axios.delete(`/api/contacts/${id}`);
@@ -67,10 +85,6 @@ const ContactState = (props) => {
 
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-
-  const updateContact = (contact) => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   const filterContacts = (text) => {
